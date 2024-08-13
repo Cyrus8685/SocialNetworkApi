8f5f5c5fd4c5f5f8b4a7cdf9fd775f2d271352dc
Jonathan Negron <jonathan.a.negron@gmail.com>
2024-08-13 17:19:51 -0400

    Updated Readme and Demonstration Video

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const { connect, connection } = require('mongoose');
 
 const connectionString =
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/socialDB';
+  process.env.MONGODB_URI || 'mongodb+srv://Cyrus:39sRx9dFzpDX8PB5@socialnetworkapi.z6o6g.mongodb.net/';
 
 connect(connectionString, {
   useNewUrlParser: true,
